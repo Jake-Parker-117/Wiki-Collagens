91671ec1711f2868a495181fae981202c0149c1c
Jake-Parker-117 <jtp.313.183@gmail.com>
2026-03-01 12:31:20 +0000

    Version 1.0 Updated logo to text, changed logo text colours, adjusted Nav margins, fixed gap char handling, removed images as no img based logo

--- Collagens.py
+++ Collagens.py
@@ -1,73 +1,142 @@
 from dotenv import load_dotenv
 from pprint import pprint
-import requests
-import os
-import re
+import requests, os, re, time, shutil
 
 load_dotenv()
 
-def fastaread(fasta_file, flattened_fasta):
-    """Opens and reads in a Fasta formatted file and flattens it
+def get_cols(fasta_file, col_txt, min_col_length=10):
     """
-    open(flattened_fasta, "w")
-    with open(fasta_file, 'r') as file:
-        with open(flattened_fasta, "w") as output:
-            for line in file:
-                if line.startswith(">"):
-                    line = '\n' + line
-                    output.write(line)
-                else:
-                    line = line.strip()
-                    output.write(line)
-            return flattened_fasta
+    Opens a fasta file, filters for collagen containing proteins and adds them to a .txt file. Deals with ambiguous chars within sequence by replacing them 
+    with an X and deals with gap (-) and terminal (*) chars by removing them from the sequence.
 
-def get_cols(flat_fasta, col_txt):
-    """Opens flattened fasta file, filters for collagen containing proteins and adds them to a .txt file
+    Args: 
+    fasta_file = Inputted fasta file
+    col_txt = text file outputted containing COL domain proteins
+    min_col_length = minimum length of COL domains permitted, measured in GXY triplets (ie. 10 triplets = 30 amino acids)
     """
-    #Using regex to find collagen containing sequences
-    header = ''
-    pattern = re.compile("(G[A-Z][A-Z]){9,}G[A-Z][A-Z]") #sets a collagen pattern with a minimum length of 27 amino acids
-    with open(flat_fasta, 'r') as f:
-        with open(col_txt, 'w') as output:
+
+    #Using regex to find collagen containing sequences and creates a txt file of all the COL domain containing proteins
+    header = None
+    new_line = [] #Using list and combining for time + memory efficiency
+    sequence = ""
+    pattern = re.compile(rf'(G[A-Z][A-Z]){{{min_col_length},}}')
+    clean1 = re.compile(r'\n|\r|\t')
+    clean2 = re.compile(r'\-|\*')
+    replaceX = re.compile(r'\s|\~|\.') #compiling regexes for time efficiency
+
+    with open(fasta_file, 'r', encoding='utf-8') as f:
+        with open(col_txt, 'w', encoding='utf-8') as output:
             for line in f:
+                line = clean1.sub("", line) #remove any special chars within or end of lines
+                if not line: #skip empty lines
+                    continue
                 if line.startswith(">"):
-                    header = line.strip()
-                
+                    if header is not None:
+                        sequence = "".join(new_line)
+                        if len(new_line)<1: #checking if the header and sequence have been separated
+                            header = clean2.sub("", header.upper()) #removing gap and terminal chars from header if header contains sequence
+                            header = replaceX.sub("X", header) #replacing ambiguous chars with "X"
+                            if pattern.search(header):
+                                output.write(f"This header and sequence were not separated\n{header}\n\n") #Saves the header/sequence the pattern is found
+                        
+                        if len(new_line)>=1:
+                            if pattern.search(sequence): #searches the combined lines for the pattern
+                                output.write(f"{header}\n{sequence}\n\n") #Saves the header and combined sequence if the pattern is found
+                        header = line #sets the new header
+                        new_line = [] #resets the new line object for next sequence
+                    else:
+                        header = line #sets 1st header
                 else:
-                    line = line.strip()
-                    regex = pattern.search(line) #searches each non-header line for that pattern
-                    if regex:
-                        output.write(f"{header} {line} \n\n") #Saves the header and sequence if the pattern is found
-            return col_txt
+                    line = clean2.sub("", line.upper()) #removing gap and terminal chars
+                    line = replaceX.sub("X", line) #replacing ambiguous amino acid chars with "X"
+                    new_line.append(line) #capturing sequence lines
+
+            if header is not None: #capturing the last input
+                sequence = "".join(new_line)
+                if len(new_line)<1:
+                    header = clean2.sub("", header.upper())
+                    if pattern.search(header):
+                        output.write(f"This header and sequence were not separated\n{header}\n\n") #Saves the header and combined sequence if the pattern is found and header and sequence wasn't separated
+                if len(new_line)>=1:
+                    if pattern.search(sequence): #searches the combined lines for the pattern
+                        output.write(f"{header}\n{sequence}\n\n")
 
 
-def label_cols(col_file, hfile):
-    """ Creates an html file with marked Col regions and interuptions within the given .txt file input
+def label_cols(col_file, hfile, table, num=12):
+    """ 
+    Creates an html file with marked COL domains and interuptions within the given .txt file input.
+    Additionally creates a tsv file which contains statistics about GXY and interuption prevelance.
+
+    Args: 
+    col_file = Inputted .txt file containing proteins with COL domains
+    hfile = html file outputted with highlighted COL domains + interuptons
+    table = tsv file output containing statistics about GXY and interuption prevelance
+    num = the maximum number of amino acids permitted to separate GXY repeats while still counting as a COL domain interuption
     """
+    
     #Using regex to identify collagen regions, marking them and then finding interuptions and marking those
     with open(col_file) as fin:
-        with open(hfile, 'w') as fout:
+        with open(hfile, 'w', encoding='utf-8') as fout:
             text = fin.read()
             
             #Marking uninterrupted Col seqs
             text = re.sub(r'(G[A-Z][A-Z]){2,}G[A-Z][A-Z]', r'</span><span class="COL">\g<0></span><span class="NC">', text)
-            
             #Marking G0G interruptions
             text = re.sub(r'G(G.)</span><span class="NC">(.)</span><span class="COL">', r'</span><span class="GXG">G</span><span class="COL">\1\2', text)
-            
             #Marking G1G interuptions
             text = re.sub(r'(G.)G</span><span class="NC">(([A-Z]){2}</span><span class="COL">)',r'</span><span class="GXG">\1</span><span class="COL">G\2', text)
-            
-            #Marking G3-15G interruptions
-            text = re.sub(r'(G..)</span><span class="NC">(([A-Z]){1,12}</span><span class="COL">)', r'\1</span><span class="GNG">\2', text)
-            
+            #Marking G3-_G interruptions
+            text = re.sub(rf'(G..)</span><span class="NC">(([A-Z]){{1,{num}}}</span><span class="COL">)', r'\1</span><span class="GNG">\2', text)
             #Marking GGG interruptions
             text = re.sub(r'GGG(G*)', r'<span class="GGG">\g<0></span>', text)
+            #Marking places where headers and sequences weren't separates
+            text = re.sub(r'(This header and sequence were not separated)', r'</span><span class="Warning">\g<0></span><span class="NC">', text)
+
+            #Counting interruptions within COL domains
+            interruption_counter = {"GGG": 0,f"G3-{num}G": 0,"G1G": 0, "G0G":0}
+            searcher = re.findall(r'<span class="GGG">|<span class="GNG">|<span class="GXG">|<span class="GXG">G<', text)
+            for t in searcher:
+                if t == '<span class="GGG">':
+                    interruption_counter["GGG"] +=1
+                elif t == '<span class="GNG">':
+                    interruption_counter[f"G3-{num}G"] +=1
+                elif t == '<span class="GXG">':
+                    interruption_counter["G1G"] +=1
+                elif t == '<span class="GXG">G<':
+                    interruption_counter["G0G"] +=1
+            with open(table, 'w') as t:
+                t.write(f"Interuptions within COL domains:\n")
+                for k,v in interruption_counter.items():
+                    t.write(f"{k}:\t{v},\t")
             
+            #Counting GXY triplets within COL domains
+            cols = ""
+            filtered_text1 = re.findall(r'<span class="COL">((?:G..)+)</span>', text) #2nd parentheses around the GXY+ unit ensures only that unit is captured while matching the whole thing
+            cols = "".join(filtered_text1)
+            triplet_counter = {}
+            total_trips = 0
+            trips = [cols[i:i+3] for i in range(0, len(cols), 3)]
+            for trip in trips:    
+                if trip not in triplet_counter:
+                    triplet_counter[trip] = 1
+                else: triplet_counter[trip] += 1
+                total_trips +=1
+            #Sorting the dictionary by values in decending order so most prevelant GXYs are at the top and adding %s after
+            triplet_counter = dict(sorted(triplet_counter.items(), key=lambda item: item[1], reverse=True))
+            for k,v in triplet_counter.items():
+                triplet_counter[k] =(v,(v/total_trips*100))
+            #Creating a TSV containing the data from the dictionary
+            with open(table, 'a', encoding='utf-8') as t:
+                t.write(f"\n\nGXY triplets in COL domains:\nGXY:\tTotal\t% of all GXYs\n")
+                for k,(v,p) in triplet_counter.items():
+                    t.write(f"{k}:\t{v}\t{round(p, 3)}%\n")
+            
+            #Creating the html file
             #Header for html file
             head = """<!DOCTYPE html>
             <html>
             <head>
+            <title>Wiki Collagens html output file</title>
             <style type="text/css">
                 p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; word-wrap: break-word; white-space: pre-wrap; font: 11.0px Menlo; color: #000000; background-color: #ffffff}
                 span.NC {font: 12.0px Menlo; word-wrap: break-word; white-space: pre-wrap; color: #000000}
@@ -75,6 +144,7 @@ def label_cols(col_file, hfile):
                 span.GXG {font: 12.0px Menlo; word-wrap: break-word; white-space: pre-wrap; color: #000000; background-color: #00ff00}
                 span.GNG {font: 12.0px Menlo; word-wrap: break-word; white-space: pre-wrap; color: #000000; background-color: #00ffff}
                 span.GGG {font: 12.0px Menlo; word-wrap: break-word; white-space: pre-wrap; color: #ff0000}
+                span.Warning {font: 12.0px Menlo; word-wrap: break-word; white-space: pre-wrap; color: #ff0000}
             </style>
             </head>
             <body>
@@ -92,15 +162,17 @@ def label_cols(col_file, hfile):
             
             fout.write(html)
 
-#if __name__ == "__main__": #This is only for running in terminal
-    #print('\n*** Find Collagens ***\n')
-    #fasta_seqs = input("Please paste your fasta sequences: ")
+def cleanup_files(folder="sessions", age_limit=3600):
+    """
+    Function to remove files that are older than 1hr
     
-   # with open("fasta_cols.fasta", "w") as fastafile:
-        #fastafile.write(fasta_seqs) #This will cause all lines of fasta to be on 1 line, but rest should be okay. Fix later
-        #with open("flat.fasta", "w") as flattened_fasta:
-            #fastaread(fastafile, flattened_fasta)
-            #with open("col.txt", "w") as col_txt_file:
-                #get_cols(flattened_fasta, col_txt_file)
-                #with open("col_html.html", "w") as htmlfile:
-                    #label_cols(col_txt_file, htmlfile)
+    Args:
+    folder = the directory within which you want to look for files older than the age limit
+    age_limit = the maximum age of a file/folder before it is deleted, measured in seconds
+    """
+    now = time.time()
+    for session in os.listdir(folder):
+        path = os.path.join(folder, session)
+        age = now - os.path.getmtime(path)
+        if age > age_limit:
+            shutil.rmtree(path)
--- main.py
+++ main.py
@@ -1,47 +1,117 @@
-from flask import Flask, render_template, request, send_from_directory
-from Collagens import fastaread, get_cols, label_cols
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for
+from flask_mail import Mail, Message
+from Collagens import get_cols, label_cols, cleanup_files
 from waitress import serve
-from werkzeug.utils import secure_filename
-import os
+from werkzeug.exceptions import HTTPException, BadRequest, NotFound
+from dotenv import load_dotenv
+import os, uuid, time, shutil
+
+load_dotenv()
 
 app = Flask(__name__)
+cleanup_files("sessions", 1)
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
-UPLOAD_DIR = "uploads"
-OUTPUT_DIR = "outputs"
-os.makedirs(UPLOAD_DIR, exist_ok=True)
-os.makedirs(OUTPUT_DIR, exist_ok=True)
+@app.route('/documentation')
+def documentation():
+    return render_template('documentation.html')
 
-@app.route("/Collagens", methods=["POST"])
-def collagens():
-    #if "fasta_seqs" not in request.files:
-        #return "Please upload a file"
+@app.route('/citation')
+def citation():
+    return render_template('citation.html')
+
+#Route for file inputs which runs all the functions given the inputs and then redirects to a different link to show the files depending on user id
+@app.route("/collagens/file_input", methods=["POST"])
+def collagens_file():
+
+    #creating random user id and creating a working directory within the sessions folder labeled as the request_id (user id)  
+    request_id = str(uuid.uuid4())
+    workdir = os.path.join("sessions", request_id)
+    os.makedirs(workdir)
     
+    #requesting input file, creating individual file within current workdir within sessions and the assigned random user id
+    cols_file = request.files['fasta_seqs']
+    input_fasta = os.path.join(workdir, "input.fasta")
+    cols_file.save(input_fasta) #save function effienctly saves the file by loading it into the other file in chuncks of 16Kb
     
-    cols = request.files['fasta_seqs']    
-    input_fasta = os.path.join(UPLOAD_DIR, "input.fasta")
-    cols.save(input_fasta)
+    #getting all inputs from web page and creating output files in working directory. Handling input errors for integers:
+    try:
+        min_col = int(request.form['col_len_min'])
+        max_inter = int(request.form['interruption_max'])
+    except(KeyError, ValueError): 
+        raise BadRequest(description="Incorrect input. Please use positive integers as inputs regarding COL domain length or interuption length.")
     
-    col_txt = os.path.join(OUTPUT_DIR, "col.txt")
-    col_html = os.path.join(OUTPUT_DIR, "col_html.html")
+    col_txt = os.path.join(workdir, "col.txt")
+    col_html = os.path.join(workdir, "col_html.html")
+    col_table = os.path.join(workdir, "col_table.txt")
     
-    fastaread(input_fasta, "flat.fasta")
-    get_cols("flat.fasta", col_txt)
-    label_cols(col_txt, col_html)
+    get_cols(input_fasta, col_txt, min_col)
+    label_cols(col_txt, col_html, col_table, max_inter)
     
-    #counting  sequences in origninal fasta file
-    with open("flat.fasta") as yfile:
-        seq_num = 0
-        for yline in yfile:
-            if yline.startswith(">"):
-                seq_num += 1
+    return redirect(url_for("results", user_id=request_id))
+
+
+#Route for file inputs which runs all the functions given the inputs and then redirects to a different link to show the files depending on user id
+@app.route("/collagens/text_input", methods=["POST"])
+def collagens_txt():
+
+    #creating random user id and creating a working directory within the sessions folder labeled as the request_id (user id)  
+    request_id = str(uuid.uuid4())
+    workdir = os.path.join("sessions", request_id)
+    os.makedirs(workdir)
     
-    #counting  sequences that are over the 27AA length requirement (ie. are in the col.txt file)
-    with open(col_txt) as xfile:
+    #requesting input text and validating it
+    cols_text_input = request.form['fasta_text']
+    input_fasta = os.path.join(workdir, "input.fasta")
+    with open(input_fasta, 'w', encoding="utf-8") as f:
+        f.write(cols_text_input)
+
+    #getting numerical inputs and handling input errors:
+    try:
+        min_col = int(request.form['col_len_min'])
+        max_inter = int(request.form['interruption_max'])
+    except(ValueError): 
+        raise BadRequest(description="Incorrect input. Please use positive integers as inputs regarding minimum COL domain length and maximuum interuption length.")
+    except(KeyError): 
+        raise BadRequest(description="Please provide inputs for minimum COL domain length and maximum interuption length.")
+    
+    #creating an input.fasta file for counting inputted sequences later and creating all the other files in the working directory
+    col_txt = os.path.join(workdir, "col.txt")
+    col_html = os.path.join(workdir, "col_html.html")
+    col_table = os.path.join(workdir, "col_table.txt")
+    
+    get_cols(input_fasta, col_txt, min_col)
+    label_cols(col_txt, col_html, col_table, max_inter)
+        
+    return redirect(url_for("results", user_id=request_id))
+
+
+
+#setting up user isolated routes per user id so that the results page url isn't the same for 2 people
+@app.route("/results/<user_id>", methods=["GET"])
+def results(user_id):
+    
+    #getting the working directory and files for counting
+    workdir = os.path.join("sessions", user_id)
+    input_fasta = os.path.join(workdir, "input.fasta")
+    col_txt = os.path.join(workdir, "col.txt")
+    
+    #counting sequences in origninal fasta file and verifying that the file still exists
+    try:
+        with open(input_fasta, 'r', encoding='utf-8') as yfile:
+            seq_num = 0
+            for yline in yfile:
+                if yline.startswith(">"):
+                    seq_num += 1
+    except:
+        raise NotFound(description="The session you are trying to access has been removed. Please run your query again.")
+    
+    #counting sequences that are over the 30AA length requirement (ie. are in the col.txt file)
+    with open(col_txt, 'r', encoding='utf-8') as xfile:
         col_num = 0
         for line in xfile:
             if line.startswith(">"):
@@ -49,14 +119,82 @@ def collagens():
     
     return render_template(
         "collagens.html",
-        title="Collagens Found!",
         seqs=seq_num,
-        collagen_num=col_num
+        collagen_num=col_num,
+        user_id=user_id,
         )
 
-@app.route("/outputs/<path:filename>")
-def outputs(filename):
-    return send_from_directory("outputs", filename)
 
-if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8000)
+
+#setting up a route for each user to view and download their files
+@app.route("/sessions/<user_id>/<filename>", methods=["GET"])
+def sessions(user_id, filename):
+    return send_from_directory(os.path.join("sessions", user_id), filename)
+
+
+
+#Error handling
+@app.errorhandler(HTTPException)
+def handle_exeption(e):
+    return render_template("errors.html",
+                            e_title=e.name,
+                            e_msg=e.description,
+                            ), e.code #returns the error code to terminal aswell as rendering to template
+
+#Configuring email sending system thru env
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
+app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
+app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+#creating mail class after configuration of Mail
+mail = Mail(app)
+
+@app.route('/message_sent')
+def message_sent():
+    return render_template('message_sent.html')
+
+#Creating mail contact form usability
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+    if request.method == 'POST':
+        #requesting details from the contact form
+        name = request.form.get('contact_name')
+        email = request.form.get('contact_email')
+        subject = request.form.get('contact_subject')
+        message = request.form.get('contact_message')
+        recipient = os.getenv('RECIPIENT')
+
+        #creating message
+        msg = Message(
+            subject=f"Wiki Collagens contact form submission",
+            recipients=[recipient],
+            body=f"""
+            New message from contact form:
+
+            Name: {name}
+            Subject: {subject}
+            email: {email}
+
+            {message}
+            """,
+            reply_to=email
+        )
+
+        #sending email and debugging if not:
+        try: 
+            mail.send(msg)
+            return redirect(url_for('message_sent'))
+        except Exception as e:
+            raise e
+    return render_template('contact.html')
+
+
+
+if __name__ == '__main__':
+    # Run Flask development server
+    app.run(debug=True, host='127.0.0.1', port=5000)
+
+    #Run Flask production server:
+    #serve(app, host=("0.0.0.0"), port=8000)
